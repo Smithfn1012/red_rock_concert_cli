3337f6d5f0491bf9f7679721ea2b99d99f9cb453
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-01-29 09:34:52 -0700

    Update scraper.rb

--- red_rock_concert_cli/lib/red_rock_concert_cli/scraper.rb
+++ red_rock_concert_cli/lib/red_rock_concert_cli/scraper.rb
@@ -1,7 +1,7 @@
 class RedRockConcertCli::Scraper
 
     def self.scrape
-      doc = Nokogiri::HTML(open("https://www.redrocksonline.com/events/category/Concerts"))
+      doc = Nokogiri::HTML(open("https://www.redrocksonline.com/concerts-events"))
   
       doc.css("div.entry.event-icons.clearfix").each do |concert|
         name = concert.css("h3").text
