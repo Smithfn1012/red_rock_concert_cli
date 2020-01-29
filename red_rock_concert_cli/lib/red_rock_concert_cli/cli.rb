f9c2a1890f396eb40a9a25af159c2542e7821907
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-01-29 09:32:31 -0700

    Update cli.rb

--- red_rock_concert_cli/lib/red_rock_concert_cli/cli.rb
+++ red_rock_concert_cli/lib/red_rock_concert_cli/cli.rb
@@ -3,68 +3,113 @@ require 'colorize'
 class RedRockConcertCli::CLI
 
   def call
+    system('clear') #clears terminal
+
     RedRockConcertCli::Scraper.scrape
     welcome
-    list_concerts
-    menu
+    main_menu_options
   end
 
   def welcome
-    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    puts "   //                                            \\\\".green.bold
-    puts "  ////                                          \\\\\\".green.bold
-    puts " /// Welcome to Red Rocks Park and Amphitheatre!! \\\\".green.bold
-    puts "////                                              \\\\".green.bold
-    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    puts "To see a current list of concerts, type list.".bold
-    puts "To leave, please type exit."
+    puts "      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ".yellow.bold
+    puts "    //                                            \\\\".green.bold
+    puts "   ////                                          \\\\\\".yellow.bold
+    puts "  /// Welcome to Red Rocks Park and Amphitheatre!! \\\\\\".green.bold
+    puts " ////                                              \\\\\\\\".yellow.bold
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".green.bold
   end
 
-  def list_concerts
-    input = gets.strip
-    if input == "list"
-      puts ""
-      concerts = RedRockConcertCli::Concert.all
-      concerts.each.with_index(1) {|concert, index| puts "#{index}. #{concert.name}"}
-    elsif input == "exit"
-      system "clear" or system "cls"
-      exit
+  def main_menu_options
+    puts "Type '1' to get a list of upcoming concerts at the Red Rocks Amphitheater!".yellow
+    puts "Type 'exit' to exit the program.".yellow
+    main_menu_input
+  end
+
+  def sub_menu_options
+    puts "Enter a concert number to get more information!".yellow
+    puts "Type 'exit' to exit program".yellow
+    sub_menu_input
+  end
+
+  def main_menu_input
+    user_input = gets.strip
+
+    if user_input == '1'
+      print_concert_details(list_concerts)
+      sub_menu_options
+    elsif user_input.downcase == "exit"
+      goodbye
     else
-      puts ""
-      puts "You can type 'exit' to leave at anytime.".bold
-      puts "To see a current list of concerts, type list."
-      list_concerts
+      invalid_choice
+      main_menu_options
     end
   end
 
-  def menu
-    puts ""
-    puts "Please select the concert number you wish to see. To leave, type 'exit'.".red.bold
-      input = gets.strip
+  def sub_menu_input
+    user_input = gets.strip
 
-    if input.to_i > 0
-      concert_choice = RedRockConcertCli::Concert.find_by_index(input.to_i - 1)
+    if user_input.to_i.between?(1, RedRockConcertCli::Concert.all.length)
+      concert = RedRockConcertCli::Concert.all[user_input.to_i - 1]
+      print_concert_details(concert)
+      continue?
+    elsif user_input.downcase == "exit"
+      goodbye
+    else
+      invalid_choice
+      sub_menu_options
+    end
+  end
+
+  def list_concerts
+    RedRockConcertCli::Concert.all.each.with_index(1) do |concert_choice, i|
+      puts "#{i}. #{concert_choice.name}"
+    end
+  end
+
+  def print_concert_details(list_concerts)
+    user_input = gets.strip
+
+    if user_input.to_i > 0
+      concert_choice = RedRockConcertCli::Concert.find_by_index(user_input.to_i - 1)
       puts ""
-      puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+      puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
       puts "Red Rocks Park & Amphitheatre is Proud to Present:".green
-      puts ""
+      puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"        
       puts "#{concert_choice.name}".bold
       puts "#{concert_choice.opener}"
       puts "On #{concert_choice.date} at #{concert_choice.time}"
       puts "For tickets and more information, please visit:".cyan
       puts "#{concert_choice.tickets_URL}"
       puts ""
-      puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-      menu
-    elsif
-      input == "exit"
-      puts ""
-      puts "Be safe out there!!".bold
-      puts ""
-      system "clear" or system "cls"
-    else
-      puts "Please try again."
-      menu
+      puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     end
+    main_menu_options
+    list_concerts
+  end
+
+  def continue?
+    puts "Type '1' for main menu, '2', to select another concert, 'exit' to exit program."
+
+    user_input = gets.strip
+    if user_input == '1'
+      main_menu_options
+    elsif user_input == '2'
+      list_concerts
+      sub_menu_options
+    elsif user_input == "exit"
+      goodbye
+    else
+      invalid_choice
+      continue?
+    end
+  end
+
+  def goodbye
+    puts "Thank you for using the RedRockConcertCli gem! Please come back soon!"
+    exit
+  end
+
+  def invalid_choice
+    puts "Please check your selection and try again!"
   end
 end
