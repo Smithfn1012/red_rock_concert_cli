65e722893984bffb02b56fbda9873732169caf56
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-01-29 09:36:35 -0700

    Update environment.rb

--- red_rock_concert_cli/config/environment.rb
+++ red_rock_concert_cli/config/environment.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 
 
-require_relative '../lib/red_rock_concert_cli/scraper'
-require_relative '../lib/red_rock_concert_cli/concert'
-require_relative '../lib/red_rock_concert_cli/cli'
 require_relative '../lib/red_rock_concert_cli/version'
+require_relative '../lib/red_rock_concert_cli/concert'
+require_relative '../lib/red_rock_concert_cli/scraper'
+require_relative '../lib/red_rock_concert_cli/cli'
